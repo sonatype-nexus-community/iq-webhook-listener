f9e75b6dba8728db8b12e064411cb56e118b01bb
CMYanko <cmyanko@gmail.com>
2019-02-01 12:23:17 -0500

    parameterized script and nopw it works with iq autocreate

--- scripts/nxrm-consume.sh
+++ scripts/nxrm-consume.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 echo "******* THIS IS A NEXUS REPO HOOK*******"
-echo $1
+echo "1 = ";$1
 echo $2
 echo $3
 echo $4
+
+AppID=${3/\//\_}
+echo $AppID
+
 cd /etc/webhook;
-docker login nexus:18444 -u admin -p admin123
-docker pull nexus:18444/$3:$4
-docker save -o docker.tar nexus:18444/$3:$4
-java -jar nexus-iq-cli.jar -s http://iq-server:8070 -i $4 -a admin:admin123 docker.tar
+docker login https://registry.mycompany.com:5000 -u admin -p admin123
+docker pull registry.mycompany.com:5000/$3:$4
+docker save -o $AppID.tar registry.mycompany.com:5000/$3:$4
+java -jar nexus-iq-cli.jar -s http://iq-server:8070 -i $AppID -t release -a admin:admin $AppID.tar
 ls -la
-rm docker.tar
-docker rmi nexus:18444/$3:$4
+rm $AppID.tar
+#docker rmi registry.mycompany.com:5000/$3:$4
 ls -la
 echo "******* END DEBUG ***********"
 
